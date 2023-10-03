a380579bf9151c0823df74681fca5e9961e15ecb
Gugor99 <121721730+Gugor99@users.noreply.github.com>
2023-10-03 10:45:25 +0200

    uploading first files colorPicker

---
+++ index.js
@@ -0,0 +1,18 @@
+const theme = document.getElementById('themes')
+const colorPicker = document.getElementById("color-picker")
+
+document.getElementById('select-btn').addEventListener('click', function(e){
+    fetch(`https://www.thecolorapi.com/scheme?hex=${colorPicker.value.substring(1)}&mode=${theme.value}&count=5`)
+        .then(res => res.json())
+        .then(data => {
+            let html = ""
+            for (obj of data.colors){
+                const colore = obj.hex.value
+                html += `<div class="section" id= "${colore}" style="background-color:${colore}">
+                            <p>${colore}</p>
+                        </div>`
+            
+        } 
+        document.getElementById("colors-container").innerHTML = html
+    })
+})
