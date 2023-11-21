cbdd75b90bf44b506d7b8089d8f2dff8688db33a
Gugor99 <121721730+Gugor99@users.noreply.github.com>
2023-11-21 09:52:21 +0100

    Committing final graphic changes

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-/*const theme = document.getElementById('themes')
-const colorPicker = document.getElementById("color-picker")
-
-document.getElementById('select-btn').addEventListener('click', function(){
-    fetch(`https://www.thecolorapi.com/scheme?hex=${colorPicker.value.substring(1)}&mode=${theme.value}&count=5`)
-    .then(res => res.json())
-    .then(data => {
-            let html = ""
-            for (obj of data.colors){
-                const colore = obj.hex.value
-                html += `  <div class="vertical-disp">
-                                <div class="section" id= "${colore}" style="background-color:${colore}">
-                                    </div>
-                                <p>${colore}</p>
-                            </div>`
-                
-            
-                        } 
-                        document.getElementById("colors-container").innerHTML = html
-    }).catch(err => document.getElementById("colors-container").innerHTML = <h1>${err}</h1>)
-})*/
-
 const option = document.getElementById('themes')
 
 const button = document.getElementById('select-btn').addEventListener('click', ()=>{
