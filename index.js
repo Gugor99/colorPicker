235b7a50f7044818460978f4ef6a612b74876190
Gugor99 <121721730+Gugor99@users.noreply.github.com>
2023-11-21 08:58:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const theme = document.getElementById('themes')
 const colorPicker = document.getElementById("color-picker")
 
-document.getElementById('select-btn').addEventListener('click', function(e){
+document.getElementById('select-btn').addEventListener('click', function(){
     fetch(`https://www.thecolorapi.com/scheme?hex=${colorPicker.value.substring(1)}&mode=${theme.value}&count=5`)
-        .then(res => res.json())
-        .then(data => {
+    .then(res => res.json())
+    .then(data => {
             let html = ""
             for (obj of data.colors){
                 const colore = obj.hex.value
@@ -15,7 +15,7 @@ document.getElementById('select-btn').addEventListener('click', function(e){
                             </div>`
                 
             
-        } 
-        document.getElementById("colors-container").innerHTML = html
-    })
+                        } 
+                        document.getElementById("colors-container").innerHTML = html
+    }).catch(err => document.getElementById("colors-container").innerHTML = <h1>${err}</h1>)
 })
