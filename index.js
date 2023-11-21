df812de59cf6afccf51ef2bdd1bc09534d399448
Gugor99 <121721730+Gugor99@users.noreply.github.com>
2023-11-21 09:17:04 +0100

    commit inicial changes after refactoring layout

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const theme = document.getElementById('themes')
+/*const theme = document.getElementById('themes')
 const colorPicker = document.getElementById("color-picker")
 
 document.getElementById('select-btn').addEventListener('click', function(){
@@ -18,4 +18,21 @@ document.getElementById('select-btn').addEventListener('click', function(){
                         } 
                         document.getElementById("colors-container").innerHTML = html
     }).catch(err => document.getElementById("colors-container").innerHTML = <h1>${err}</h1>)
+})*/
+
+const option = document.getElementById('themes')
+
+const button = document.getElementById('select-btn').addEventListener('click', ()=>{
+    const color = document.getElementById('color-picker').value.substring(1)
+    const mode = option[option.selectedIndex].value
+    fetch(`https://www.thecolorapi.com/scheme?hex=${color}&mode=${mode}`)
+        .then(result => result.json())
+        .then(data => {
+            for (let i = 0; i <data.colors.length; i++) {
+                let hexValue = data.colors[i].hex.value;
+                document.getElementById(`hex-${i + 1}`).textContent = hexValue;
+                document.getElementById(`color-${i + 1}`).style.backgroundColor = hexValue;
+            }
+        })
+        .catch(error => console.error('Something went wrong fetching data:', error));
 })
